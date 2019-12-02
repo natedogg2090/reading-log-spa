487e24817aec752092a405395eada9092f94b418
Nate Dietz <nsdietz@gmail.com>
2019-12-02 13:51:39 -0500

    create book method to configure fetch header and send book data

--- js-frontend/src/adapters/BooksAdapter.js
+++ js-frontend/src/adapters/BooksAdapter.js
@@ -6,4 +6,19 @@ class BooksAdapter {
 	getBooks() {
 		return fetch(this.baseUrl).then(res => res.json())
 	}
+
+	createBook(title, summary) {
+		const book = {
+			title: title,
+			summary: summary
+		}
+
+		return fetch(this.baseUrl, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({ book })
+		}).then(res => res.json())
+	}
 }
