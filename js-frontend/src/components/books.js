9e29587fa58574fab7ff53972ca2754914e49000
Nate Dietz <nsdietz@gmail.com>
2019-12-02 13:11:28 -0500

    instantiating new book class on returned data and pushing into array

--- js-frontend/src/components/books.js
+++ js-frontend/src/components/books.js
@@ -9,8 +9,7 @@ class Books {
 		this.adapter
 		.getBooks()
 		.then(books => {
-			// books.forEach(book => this.books.push(book)) // pushing a new book instance into the array
-			console.log("fetching books...")
+			books.forEach(book => this.books.push(new Book(book))) // pushing a new book instance into the array
 		})
 		.then(() => {
 			console.log(this.books)
