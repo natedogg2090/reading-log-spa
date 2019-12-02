6ebf8223a47d814b34d2c622010a56a7738ed51a
Nate Dietz <nsdietz@gmail.com>
2019-12-02 13:04:45 -0500

    created method to call BooksAdapter and initiate fetch request for Books

--- js-frontend/src/components/books.js
+++ js-frontend/src/components/books.js
@@ -2,5 +2,18 @@ class Books {
 	constructor() {
 		this.books = []
 		this.adapter = new BooksAdapter()
+		this.fetchAndLoadBooks()
+	}
+
+	fetchAndLoadBooks() {
+		this.adapter
+		.getBooks()
+		.then(books => {
+			// books.forEach(book => this.books.push(book)) // pushing a new book instance into the array
+			console.log("fetching books...")
+		})
+		.then(() => {
+			console.log(this.books)
+		})
 	}
 }
