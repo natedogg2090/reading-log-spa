ae131182da4b17cf2cdc67deb75b64ae887ca659
Nate Dietz <nsdietz@gmail.com>
2019-12-02 11:46:22 -0500

    added show method to books controller

--- reading-log-api-backend/app/controllers/api/v1/books_controller.rb
+++ reading-log-api-backend/app/controllers/api/v1/books_controller.rb
@@ -4,4 +4,11 @@ class Api::V1::BooksController < ApplicationController
 
     render json: @books, status: 200
   end
+
+  def show
+    @book = Book.find(params[:id])
+
+    render json: @book, status: 200
+  end
+
 end
