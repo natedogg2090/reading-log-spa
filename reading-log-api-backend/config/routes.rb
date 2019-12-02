ad3ece7045ed5ba9da4fd2bdd0b93424dacff341
Nate Dietz <nsdietz@gmail.com>
2019-12-02 14:06:12 -0500

    added create route

--- reading-log-api-backend/config/routes.rb
+++ reading-log-api-backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show]
+      resources :books, only: [:index, :show, :create]
     end
   end
 end
