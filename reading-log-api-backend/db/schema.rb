12cacc54b0ebe4badf133a2ed45705f48b075808
Nate Dietz <nsdietz@gmail.com>
2019-12-03 14:02:47 -0500

    created genre model

--- reading-log-api-backend/db/schema.rb
+++ reading-log-api-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_182528) do
+ActiveRecord::Schema.define(version: 2019_12_03_185750) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2019_12_03_182528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
