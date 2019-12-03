4cf2a824740bfa58210da1c8628f2f03da220fa5
Nate Dietz <nsdietz@gmail.com>
2019-12-03 14:04:00 -0500

    migrated db

--- reading-log-api-backend/db/schema.rb
+++ reading-log-api-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_185750) do
+ActiveRecord::Schema.define(version: 2019_12_03_190059) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_185750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+    t.integer "genre_id"
   end
 
   create_table "genres", force: :cascade do |t|
