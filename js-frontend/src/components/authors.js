d467fa422d888d36c01d5e21dd1fa7a69312ab26
Nate Dietz <nsdietz@gmail.com>
2019-12-03 15:18:22 -0500

    added fetch method to get Authors

--- js-frontend/src/components/authors.js
+++ js-frontend/src/components/authors.js
@@ -4,4 +4,17 @@ class Authors {
 		this.adapter = new AuthorsAdapter()
 		this.fetchAuthors()
 	}
+
+	fetchAuthors() {
+		this.adapter
+		.getAuthors()
+		.then(authors => {
+			authors.forEach(author => this.authors.push(new Author(author))) // pushing a new book instance into the array
+		})
+		.then(() => {
+			// this.sortBooks()
+			// this.renderBooks()
+			console.log(this.authors)
+		})
+	}
 }
