dd2667fa945489dfd9b1c9ccb168974d5ef066bb
Nate Dietz <nsdietz@gmail.com>
2019-12-04 13:24:37 -0500

    authors now include books

--- reading-log-api-backend/app/controllers/api/v1/authors_controller.rb
+++ reading-log-api-backend/app/controllers/api/v1/authors_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::AuthorsController < ApplicationController
   def index
     @authors = Author.all
 
-    render json: @authors, status: 200
+    render json: @authors, include: [:books], status: 200
   end
 end
