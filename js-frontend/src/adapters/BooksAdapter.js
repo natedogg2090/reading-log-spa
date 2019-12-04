4a854c6c1a3055d81fdb98a5cf2cafcd54ad3ae2
Nate Dietz <nsdietz@gmail.com>
2019-12-04 12:43:40 -0500

    added arguments to fetch call passing author and genre

--- js-frontend/src/adapters/BooksAdapter.js
+++ js-frontend/src/adapters/BooksAdapter.js
@@ -7,10 +7,18 @@ class BooksAdapter {
 		return fetch(this.baseUrl).then(res => res.json())
 	}
 
-	createBook(title, summary) {
+	createBook(title, summary, name, genreName) {
 		const book = {
 			title: title,
 			summary: summary
+		}
+
+		const author = {
+			name: name
+		}
+
+		const genre = {
+			name: genreName
 		}
 
 		return fetch(this.baseUrl, {
@@ -18,7 +26,7 @@ class BooksAdapter {
 			headers: {
 				'content-type': 'application/json',
 			},
-			body: JSON.stringify({ book })
+			body: JSON.stringify({ book, author, genre })
 		}).then(res => res.json())
 	}
 }
