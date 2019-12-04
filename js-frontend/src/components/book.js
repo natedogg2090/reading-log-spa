5aad5eaeadea037b78f8afdbcba73cc13855f4cb
Nate Dietz <nsdietz@gmail.com>
2019-12-04 11:19:21 -0500

    added element for author name to renderBook method

--- js-frontend/src/components/book.js
+++ js-frontend/src/components/book.js
@@ -3,6 +3,7 @@ class Book {
 		this.id = bookJSON.id
 		this.title = bookJSON.title
 		this.summary = bookJSON.summary
+		this.author = bookJSON.author.name
 	}
 
 	renderBook() {
@@ -10,19 +11,23 @@ class Book {
 		let div = document.createElement('div')
 		div.setAttribute('class', 'single-book')
 
-		let internalDiv = document.createElement('div')
-		internalDiv.setAttribute('class', 'content')
+		let content = document.createElement('div')
+		content.setAttribute('class', 'content')
 
 		let header = document.createElement('h3')
 		header.innerHTML = `${this.title}`
 
+		let authorName = document.createElement('h4')
+		authorName.innerHTML = `${this.author}`
+
 		let p = document.createElement('p')
 		p.innerHTML = `${this.summary}`
 
-		internalDiv.appendChild(header)
-		internalDiv.appendChild(p)
+		content.appendChild(header)
+		content.appendChild(p)
+		content.appendChild(authorName)
 
-		div.appendChild(internalDiv)
+		div.appendChild(content)
 
 		return div
 		// return `<h3>${this.title}</h3>`
