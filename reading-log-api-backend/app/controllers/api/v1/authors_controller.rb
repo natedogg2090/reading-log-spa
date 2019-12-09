5a65819e9aa353e4f58add5e63fed6077224ebda
Nate Dietz <nsdietz@gmail.com>
2019-12-09 14:01:17 -0500

    added show method

--- reading-log-api-backend/app/controllers/api/v1/authors_controller.rb
+++ reading-log-api-backend/app/controllers/api/v1/authors_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::AuthorsController < ApplicationController
 
     render json: AuthorSerializer.new(authors), status: 200
   end
+
+  def show
+    author = Author.find(params[:id])
+
+    render json: AuthorSerializer.new(author), status: 200
+  end
 end
