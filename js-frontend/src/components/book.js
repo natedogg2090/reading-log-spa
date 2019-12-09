fb48e8873e744b0bfca3147772f6070d3c2e9d67
Nate Dietz <nsdietz@gmail.com>
2019-12-09 12:03:36 -0500

    modified renderBooks method to return author with href and make the container clickable to the book show page

--- js-frontend/src/components/book.js
+++ js-frontend/src/components/book.js
@@ -3,11 +3,12 @@ class Book {
 		this.id = bookJSON.id
 		this.title = bookJSON.attributes.title
 		this.summary = bookJSON.attributes.summary
-		this.author = bookJSON.attributes.author.name
+		this.author = bookJSON.attributes.author
 	}
 
 	renderBook() {
 		// return `<input type="checkbox" name="book-${this.id}" value="false">${this.title}<br>`
+
 		let div = document.createElement('div')
 		div.setAttribute('class', 'single-book')
 
@@ -17,19 +18,30 @@ class Book {
 		let header = document.createElement('h3')
 		header.innerHTML = `${this.title}`
 
+		let a = document.createElement('a')
+		a.setAttribute('href', `/${this.author.id}`)
+		a.innerHTML = `${this.author.name}`
+
 		let authorName = document.createElement('h4')
-		authorName.innerHTML = `${this.author}`
+		// authorName.innerHTML = `${this.author.name}`
+
+		let sumDiv = document.createElement('div')
+		sumDiv.setAttribute('class', 'book-summary')
 
 		let p = document.createElement('p')
 		p.innerHTML = `${this.summary}`
 
 		content.appendChild(header)
-		content.appendChild(authorName)
-		content.appendChild(p)
+		authorName.appendChild(a)
+		// content.appendChild(p)
 
 		div.appendChild(content)
+		div.appendChild(authorName)
+		sumDiv.appendChild(p)
+		div.appendChild(sumDiv)
 
 		return div
 		// return `<h3>${this.title}</h3>`
 	}
+
 }
